d6fd82eda6fea051db756e7b3051d95cb24d7197
Federico Garza <fede.garza.ramirez@gmail.com>
2020-02-07 13:50:41 -0600

    author

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="tsfeatures-FedericoGarza", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Federico Garza",
+    author_email="fede.garza.ramirez@gmail.com",
+    description="features for time series",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/FedericoGarza/tsfeatures",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

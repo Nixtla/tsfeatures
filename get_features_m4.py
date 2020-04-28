55432ea43437a288810395dc450d2dbfede0847b
Fede <fede.garza.ramirez@gmail.com>
2020-04-28 14:21:40 -0500

    using datasetname as argument

--- get_features_m4.py
+++ get_features_m4.py
@@ -15,13 +15,13 @@ def get_features_m4(dataset_name, directory, num_obs=1000000, parallel=True):
     _, y_train_df, _, _ = prepare_m4_data(dataset_name=dataset_name,
                                           directory = directory,
                                           num_obs=num_obs)
-    
+
     freq = seas_dict[dataset_name]['seasonality']
     feats = tsfeatures(y_train_df, freq=freq, parallel=parallel)
     feats = feats.rename_axis('unique_id')
-    
+
     feats.to_csv('{}/results/{}-features.csv'.format(directory, dataset_name))
-    
+
     print('Features saved')
 
 def main(args):
@@ -29,7 +29,13 @@ def main(args):
         num_obs = args.num_obs
     else:
         num_obs = 100000
-    for dataset_name in seas_dict.keys():
+
+    if args.dataset_name:
+        datasets = [args.dataset_name]
+    else:
+        datasets = seas_dict.keys()
+
+    for dataset_name in datasets:
         get_features_m4(dataset_name, args.results_directory, num_obs)
 
 
@@ -39,10 +45,8 @@ if __name__=='__main__':
                         help="directory where M4 data will be downloaded")
     parser.add_argument("--num_obs", required=False, type=int,
                         help="number of M4 time series to be tested (uses all data by default)")
+    parser.add_argument("--dataset_name", required=False, type=str,
+                        help="type of dataset to get features")
     args = parser.parse_args()
 
     main(args)
-
-
-
-

8e0a64705052876d3582566eda2806f507718bca
Fede <fede.garza.ramirez@gmail.com>
2020-06-18 20:16:25 -0500

    removing comparison dir (now includes a module)

--- comparison/get_py_features.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from tsfeatures import tsfeatures
-
-series = pd.read_csv('sample_series.csv')
-
-features = tsfeatures(series, 7)
-features.index.name = 'unique_id'
-features.to_csv('pyfeatures.csv')

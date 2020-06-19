eeb544c86504d9e00867c631fda9f2e86cfb5084
Fede <fede.garza.ramirez@gmail.com>
2020-06-19 18:34:29 -0500

    fixing hw_parameters behavior

--- tsfeatures/tsfeatures_r.py
+++ tsfeatures/tsfeatures_r.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-try:
-    import rpy2.robjects as robjects
-    from rpy2.robjects import pandas2ri
-except ImportError:
-    raise ImportError('rpy2 package not found, please install it.')
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
 
-def tsfeatures_r(ts, freq, **kwargs):
+def tsfeatures_r(ts, freq,
+                 features = ["length",
+                            "acf_features",
+                            "arch_stat",
+                            "crossing_points",
+                            "entropy",
+                            "flat_spots",
+                            "heterogeneity",
+                            "holt_parameters",
+                            "hurst",
+                            "hw_parameters",
+                            "lumpiness",
+                            "nonlinearity",
+                            "pacf_features",
+                            "stability",
+                            "stl_features",
+                            "unitroot_kpss",
+                            "unitroot_pp"],
+                 **kwargs):
     """tsfeatures wrapper using r.
 
     Parameters
@@ -16,19 +31,19 @@ def tsfeatures_r(ts, freq, **kwargs):
         Pandas DataFrame with columns ['unique_id', 'ds', 'y']
     freq: int
         Frequency of the time series.
+    features: List[str]
+        String list of features to calculate.
     **kwargs:
-        Arguments passed to the original tsfeatures function.
+        Arguments used by the original tsfeatures function.
 
     References
     ----------
     https://pkg.robjhyndman.com/tsfeatures/reference/tsfeatures.html
     """
     rstring = """
-        function(df, freq, ...){
+        function(df, freq, features, ...){
           suppressMessages(library(data.table))
           suppressMessages(library(tsfeatures))
-
-          set.seed(12398)
 
           dt <- as.data.table(df)
           setkey(dt, unique_id)
@@ -37,38 +52,36 @@ def tsfeatures_r(ts, freq, **kwargs):
           series_list <- lapply(series_list,
                                 function(serie) serie[, ts(y, frequency = freq)])
 
-          features <-
-            c("length",
-              "acf_features",
-              "arch_stat",
-              "crossing_points",
-              "entropy",
-              "flat_spots",
-              "heterogeneity",
-              "holt_parameters",
-              "hurst",
-              "hw_parameters",
-              "lumpiness",
-              "nonlinearity",
-              "pacf_features",
-              "stability",
-              "stl_features",
-              "unitroot_kpss",
-              "unitroot_pp")
+          if("hw_parameters" %in% features){
+            features <- setdiff(features, "hw_parameters")
 
-          series_features <- tsfeatures(series_list)#, features = features)
+            if(length(features)>0){
+                hw_series_features <- suppressMessages(tsfeatures(series_list, "hw_parameters", ...))
+                names(hw_series_features) <- paste0("hw_", names(hw_series_features))
+
+                series_features <- suppressMessages(tsfeatures(series_list, features, ...))
+                series_features <- cbind(series_features, hw_series_features)
+            } else {
+                series_features <- suppressMessages(tsfeatures(series_list, "hw_parameters", ...))
+                names(series_features) <- paste0("hw_", names(series_features))
+            }
+          } else {
+            series_features <- suppressMessages(tsfeatures(series_list, features, ...))
+          }
 
           setDT(series_features)
 
           series_features[, unique_id := names(series_list)]
+
         }
     """
-
     pandas2ri.activate()
     rfunc = robjects.r(rstring)
 
-    feats = rfunc(ts, freq, **kwargs)
-
+    feats = rfunc(ts, freq, features, **kwargs)
     feats = feats.set_index('unique_id')
+
+    renamer={'ARCH.LM': 'arch_lm', 'length': 'series_length'}
+    feats = feats.rename(columns=renamer)
 
     return feats

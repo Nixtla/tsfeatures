4631efe98cf9cdfbfa3372f0ddca554d91b0290c
Fede <fede.garza.ramirez@gmail.com>
2020-08-03 15:22:36 -0500

    adding new entropy link

--- setup.py
+++ setup.py
@@ -19,10 +19,10 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=[
         "arch>=4.11",
+        "entropy @ https://github.com/raphaelvallat/entropy/tarball/tag/v0.1.1"
         "pandas>=1.0.5",
         "scikit-learn>=0.23.1",
         "statsmodels>=0.11.1",
         "supersmoother>=0.4"
-    ],
-    dependency_links=['https://github.com/raphaelvallat/entropy/tarball/tag/v0.1.1']
+    ]
 )

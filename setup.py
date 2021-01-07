4e9cd993e763d5a39b462a2d980b7ebf8a4a18dd
Fede <fede.garza.ramirez@gmail.com>
2021-01-07 13:44:00 -0600

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsfeatures",
-    version="0.1.0",
+    version="0.2.0",
     description="Calculates various features from time series data.",
     long_description=long_description,
     long_description_content_type="text/markdown",

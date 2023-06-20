351ef5853169da1e846b730e37bfaff69a57556c
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-06-20 01:17:24 -0700

    feat: add test for series with 1 obs

--- tsfeatures/tests/test_small_ts.py
+++ tsfeatures/tests/test_small_ts.py
@@ -5,7 +5,7 @@ from tsfeatures import (
     entropy, flat_spots, heterogeneity, holt_parameters,
     lumpiness, nonlinearity, pacf_features, stl_features,
     stability, hw_parameters, unitroot_kpss, unitroot_pp,
-    series_length, sparsity, hurst
+    series_length, sparsity, hurst, statistics
 )
 
 
@@ -17,8 +17,20 @@ def test_small():
               entropy, flat_spots, holt_parameters,
               lumpiness, nonlinearity, pacf_features, stl_features,
               stability, hw_parameters, unitroot_kpss, unitroot_pp,
-              series_length, hurst]
+              series_length, hurst, statistics]
+    feats_df = tsfeatures(z_df, freq=12, features=feats, scale=False)
+
+def test_small_1():
+    z = np.zeros(1)
+    z[-1] = 1
+    z_df = pd.DataFrame({'unique_id': 1, 'ds': range(1, 2), 'y': z})
+    feats=[sparsity, acf_features, arch_stat, crossing_points,
+              entropy, flat_spots, holt_parameters,
+              lumpiness, nonlinearity, pacf_features, stl_features,
+              stability, hw_parameters, unitroot_kpss, unitroot_pp,
+              series_length, hurst, statistics]
     feats_df = tsfeatures(z_df, freq=12, features=feats, scale=False)
 
 if __name__=="__main__":
     test_small()
+    test_small_1()

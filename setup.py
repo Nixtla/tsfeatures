5e03313a45da66f2f5fc5ec03d976db0d59ac2f8
jope35 <15650945+jope35@users.noreply.github.com>
2024-01-10 17:04:43 +0100

    restore setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tsfeatures",
+    version="0.4.5",
+    description="Calculates various features from time series data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nixtla/tsfeatures",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "antropy>=0.1.4",
+        "arch>=4.11",
+        "pandas>=1.0.5",
+        "scikit-learn>=0.23.1",
+        "statsmodels>=0.13.2",
+        "supersmoother>=0.4"
+    ]
+)
